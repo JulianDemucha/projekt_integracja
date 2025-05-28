6590512f49d89af99aee47e8859d8da9901fc52c
Julian Demucha <demuchajulian.it@gmail.com>
2025-05-28 15:44:22 +0200

    removed unnecessary imports from main application class

--- backend/covid_stats/src/main/java/com/covid_stats/covid_stats/CovidStatsApplication.java
+++ backend/covid_stats/src/main/java/com/covid_stats/covid_stats/CovidStatsApplication.java
@@ -1,12 +1,6 @@
 package com.covid_stats.covid_stats;
-
-import com.covid_stats.covid_stats.Models.AppUser;
-import com.covid_stats.covid_stats.Repositories.AppUserRepo;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class CovidStatsApplication {
